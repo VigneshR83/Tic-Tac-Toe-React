f4d43e97c50531fe541f718d25575d42766fdcbc
VIgnesh Ravikumar <142441342+VigneshR83@users.noreply.github.com>
2024-05-11 14:35:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TikTacToe from './Components/TikTacToe/TikTacToe.jsx'
+
+function App() {
+  return (
+    <div>
+     <TikTacToe />
+    </div>
+  );
+}
+
+export default App;
